6a0b75017ce5303f10f3f01d78e7cb8422db0a1c
hasanaliarikannn <hasanaliarikan077@hotmail.com>
2021-03-23 14:37:48 +0300

    first commit

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,Tray,nativeImage } = require('electron')
 const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+   
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadUrl('http://localhost:3000')
+
+  win.loadURL('http://localhost:3000')
 }
+
+
 
 app.whenReady().then(() => {
   createWindow()
--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+import moment from "moment";
+
+import "moment/locale/tr";
+
+moment.locale("tr");
 
 function App() {
+  const [bilgi, setBilgi] = useState("");
+  const [ölüm, setÖlüm] = useState("");
+  const [enfekte, setEnfekte] = useState("");
+  const [recovered, Setrecovered] = useState("");
+
+  const [günlükenfekte,setGünlükenfekte]=useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const url =
+        "https://api.apify.com/v2/key-value-stores/28ljlt47S5XEd1qIi/records/LATEST?disableRedirect=true";
+
+      try {
+        const result = await axios.get(url);
+
+        if (result.data) {
+          setBilgi(result);
+          setÖlüm(result);
+          setEnfekte(result);
+          Setrecovered(result);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h2> En son günceleme {moment().format("MMMM Do YYYY, h:mm:ss ")} </h2>
+
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Türkiye toplam enfekte kişi sayısı :{" "}
+          {enfekte ? enfekte.data.infected : <h1>Yükleniyor</h1>}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>
+          Türikye Toplam Ölü kişi sayısı :{" "}
+          {ölüm ? ölüm.data.deceased : <h1>Yükleniyor</h1>}
+        </p>
+        <p>
+          Türkiye kurtarılan toplam kişi:
+          {recovered ? recovered.data.recovered : <h1>Yükleniyor</h1>}
+        </p>
+      </div>
     </div>
   );
 }
-
 export default App;
